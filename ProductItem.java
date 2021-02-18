ea25c13d81192cb6804c89daf1ffdffab5379f27
mtesomo <tesomo58@yahoo.com>
2021-02-18 16:48:38 -0600

    new file

--- ProductItem.java
+++ ProductItem.java
@@ -1,18 +1,22 @@
 public class ProductItem {
-    public String tartina; double price; int quantity;
-    public double totalPrice(int quantity,double price){
-        double totalPrice = quantity*price;
-        System.out.println(totalPrice);
-         return(totalPrice);
+    public String item;
+    double price;
+    int quantity;
+
+    public double getTotalPrice(){
+        double getTotalPrice = quantity*price;
+        System.out.println(getTotalPrice);
+         return(getTotalPrice);
      }
 
-     public static void main(String[] args){
-        int quantity = 5; double price= 7.52;
-        ProductItem tartina = new ProductItem();
-        tartina.totalPrice(quantity,price);
-         System.out.println("the total price of all the tartina cans is " + tartina.totalPrice(5,7.52));
+    public static void main(String[] args){
+       ProductItem tartina = new ProductItem();
+       tartina.price= 5.8;
+       tartina.quantity = 46;
+        System.out.println("the total price of all the tartina cans is " + tartina.getTotalPrice());
 
-     }
+    }
+
 
 }
 
